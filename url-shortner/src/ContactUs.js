d6dbc84337ec6e3fed6c25c723012298a68856cb
M-Rahim <muhammadrahim99x@gmail.com>
2023-05-05 19:18:48 +0500

    contactUs Page

--- url-shortner/src/ContactUs.js
+++ url-shortner/src/ContactUs.js
@@ -6,11 +6,25 @@ function ContactUS () {
   return(
 <div className="ContactUs">
  
- <h2> Contact  US</h2>
-  
- <p className='about-para'>
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus sint et laboriosam rerum, repudiandae ipsum dolores vitae, voluptas quidem ducimus delectus suscipit. Culpa quidem consectetur, laborum quo cum est eum repellat amet porro quisquam! Minus perferendis aperiam animi hic consectetur illo fuga earum, consequuntur quas similique qui accusantium, placeat debitis!
-      </p> 
+<h1><b className="contact_color">CONTACT</b> INFORMATION</h1>
+      <h4 className="contact_second">Get In Touch With Us</h4>
+      <h3 className="contact_second">Shorten your links,<b className="coler"> simplify your life</b></h3>
+      <div className='main-flex'>
+
+        <div className="col-sm-4" id='information' ><h5> <b className="coler">A</b>DDRESS</h5>
+        <div>Askari Corporate Towers, 12th Floor, 75- Block D1, Gulberg –III, LAHORE, Punjab, 54660, Pakistan</div>
+        </div>
+        
+        <div className="col-sm-4"  id='information'><h5> <b className="coler">P</b>HONE</h5>
+        <div>+92 42 35788961 </div>
+        <div> +92 42 35788963</div>
+        </div>
+        
+        <div className="col-sm-4" id='information'><h5> <b className="coler">M</b>AIL</h5>
+        <div>info@contour-software.com</div>
+        </div>
+        
+      </div>
   </div>
   );
 }
